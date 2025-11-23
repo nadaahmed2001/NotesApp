d2bb302bc31c4ebb91f3b37e9be399f2bb3cdc3a
Nada Ahmed <nada052001@gmail.com>
2025-11-23 19:57:45 +0200

    Navbar.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,18 +6,12 @@ import NoteDetailPage from './pages/NoteDetailPage';
 
 function App() {
   return (
-    <div>
-      <div data-theme="forest">
-        <button class="btn btn-primary">One</button>
-        <button class="btn btn-secondary">Two</button>
-        <button class="btn btn-accent btn-outline">Three</button>
-      </div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/note/:id" element={<NoteDetailPage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/note/:id" element={<NoteDetailPage />} />
+    </Routes>
+
   )
 }
 
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import toast from 'react-hot-toast'
+import Navbar from '../components/Navbar'
 
 
 const HomePage = () => {
     return (
-        <div>
-            HomePage
+        <div className='="min-h-screen'>
+            <Navbar/>
+            
         </div>
     )
 }
