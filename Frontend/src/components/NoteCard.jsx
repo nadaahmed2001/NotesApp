bcabab48aa60914b50f3bb01b35fc5fdbf93a048
Nada Ahmed <nada052001@gmail.com>
2026-01-04 13:29:57 +0200

    Edit and delete note - frontend

--- Frontend/src/components/NoteCard.jsx
+++ Frontend/src/components/NoteCard.jsx
@@ -1,12 +1,27 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
-// import api from "../lib/axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+    const handleDelete = async (e, id) => {
+        e.preventDefault(); //prevents the default behavior of the button (go to the note page)
+        if (!window.confirm("Are you sure you want to delete this note?")) return;
+        try {
+            const response = await api.delete(`/notes/${id}`);
+            // console.log(response);
+            toast.success("Note deleted successfully");
+            //“Update the notes state by removing the deleted note from the array.”
+            setNotes((prevNotes) => prevNotes.filter((note) => note._id !== id));
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Could not delete note");
+        }
+    }
     return (
-        <Link to={`/note/${note.id}`}
+        <Link to={`/note/${note._id}`}
             className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#7480ff]"
         >
             <div className="card-body">
@@ -17,9 +32,13 @@ const NoteCard = ({ note }) => {
                         {formatDate(new Date(note.createdAt))}
                     </span>
                     <div className="flex items-center gap-1">
-                        <PenSquareIcon className="size-4" />
+                        {/* Edit button */}
+                        <PenSquareIcon className="size-4"
+
+                        />
+                        {/* Delete buttun */}
                         <button className="btn btn-ghost btn-xs text-error"
-                        // onClick={(e) => handleDelete(e, note._id)}
+                            onClick={(e) => handleDelete(e, note._id)}
                         >
                             <Trash2Icon className="size-4" />
                         </button>
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import toast from 'react-hot-toast'
 import Navbar from '../components/Navbar'
 import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound';
 import RateLimitedUI from './RateLimitedUI';
 import { useState } from 'react';
 import axios from 'axios';
+
 
 
 const HomePage = () => {
@@ -13,11 +15,11 @@ const HomePage = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const getNotes = async () => {
+        const getNotes = async (set) => {
             try {
-                console.log("Fetching notes...");
+                // console.log("Fetching notes...");
                 const res = await axios.get("http://localhost:5001/api/notes")
-                console.log("Notes: ", res.data);
+                // console.log("Notes: ", res.data);
                 setNotes(res.data);
                 setIsRateLimited(false);
             } catch (error) {
@@ -43,11 +45,19 @@ const HomePage = () => {
 
             {/* Notes */}
             <div className='max-w-7xl mx-auto p-4 mt-6'>
+
+                {/* //Loading */}
                 {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+
+                {/* //No notes */}
+                {!loading && notes.length===0 && !isRateLimited && <NotesNotFound/>}
+
+                {/* //Notes fetched */}
                 {notes.length > 0 && !isRateLimited && (
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                         {notes.map(note => (
-                            <NoteCard key={note._id} note={note} />
+                            <NoteCard key={note._id} note={note} setNotes={setNotes} />
+                            //set notes to empty array after deleting a note
                         ))}
 
                     </div>
