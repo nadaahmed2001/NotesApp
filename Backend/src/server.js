f82fdd068085c3f307502864a05d3ad045471aaa
Nada Ahmed <nada052001@gmail.com>
2026-01-06 12:47:41 +0200

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,58 +1,44 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// const express = require("express");
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import ratelimiter from "./middleware/ratelimiter.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5001;
-// console.log(process.env.MONGO_URI);
-const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);//returns the backend directory
 
-//middleware
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://notes-app-ecru-six.vercel.app",
-        "https://notesapp-0asj.onrender.com"
-    ],
-    credentials: true
-}));
-app.use(express.json());
-app.use(ratelimiter)
-app.use("/api/notes", notesRoutes);
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+// middleware
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+
+app.use("/api/notes", notesRoutes);//this will handle all the routes starting with /api/notes
 
 if (process.env.NODE_ENV === "production") {
+    //serve static files
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    const frontendDistPath = path.resolve(__dirname, "..", "..", "Frontend", "dist");
-
-    //returns the frontend/dist directory for serving static files
-    //serving static files means serving the built React app (frontend)
-    app.use(express.static(frontendDistPath));
-
-
-    //if we got any route other than API routes, serve the React app
     app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendDistPath, "index.html"));
+        //serve the index.html file
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-
 }
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on PORT:", PORT);
     });
 });
-
-
-
-
-
-
